1daee9fa1ae46d96f417c5fe6b156aa6025d8ecd
GuitarCYQ <1159543426@qq.com>
2020-11-05 00:57:57 +0000

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::resource('users','UsersController', ['only' => ['show','update','edit']])
 //话题
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 //图片上传
-Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 //分类
 Route::resource('categories','CategoriesController', ['only' => ['show']]);
