6ac44974164fe1d43443440387edadf31106e667
GuitarCYQ <1159543426@qq.com>
2020-11-13 12:08:22 +0000

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Http\Controllers\PagesController;
 use App\Models\User;
 
 // creating, created, updating, updated, saving,
@@ -9,13 +10,11 @@ use App\Models\User;
 
 class UserObserver
 {
-    public function creating(User $user)
+    public function saving(User $user)
     {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
+        //这样写扩展性更高 只有空的时候才指定默认头像
+        if (empty($user->avatar)){
+            $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
 }
